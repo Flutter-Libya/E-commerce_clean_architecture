1318fde6dd96c98929c77eb86353ec89cc0c0e53
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-10-05 10:51:04 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
-
+import 'config/theme/my_theme.dart';
 import 'app/data/local/my_shared_pref.dart';
 import 'app/routes/app_pages.dart';
-import 'config/theme/my_theme.dart';
+
 
 Future<void> main() async {
-  // wait for bindings
   WidgetsFlutterBinding.ensureInitialized();
-
-  // init shared preference
   await MySharedPref.init();
-
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
@@ -32,8 +27,8 @@ Future<void> main() async {
             return Theme(
               data: MyTheme.getThemeData(isLight: themeIsLight),
               child: MediaQuery(
-                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
                 child: widget!,
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
               ),
             );
           },

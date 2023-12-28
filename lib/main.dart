e9eedacf5397990cd1ed20bfba21d58805ee9b48
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-12-28 18:27:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'config/theme/my_theme.dart';
 import 'app/data/local/my_shared_pref.dart';
 import 'app/routes/app_pages.dart';
-
-
 
 
 Future<void> main() async {
